2d1941fe6a7d8496bf9f67a24cdfae98682b48da
sumaya2791 <mahmudasumaya023@gmail.com>
2024-09-02 00:36:10 +0600

    Create 5a.java

---
+++ 5a.java
@@ -0,0 +1,19 @@
+public class problem5a {
+    public static void main(String[] args) {
+  
+      // size of the triangle
+      int size = 3;
+      // loop to print the pattern
+      for (int i = 0; i < size; i++) {
+        // print spaces
+        for (int j = 1; j < size - i; j++) {
+          System.out.print(" ");
+        }
+        // print stars
+        for (int k = 0; k <= i; k++) {
+          System.out.print("*");
+        }
+        System.out.println();
+      }
+    }
+  }
